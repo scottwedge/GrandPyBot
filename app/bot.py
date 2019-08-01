4da44bf5c51effee4ee6ad2bfc3346040c3c0634
Younes <boukroun.younes@gmail.com>
2019-08-01 17:19:56 +0200

    Tests Unittest importations problems, import the bot.py from the app pack

---
+++ app/bot.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import render_template, request
+import urllib.parse
+import requests
+import random
+
+
+class bot():
+    """
+    Class to make the bots logics, and methods to communicate 
+    with the front end querys.
+    """
+    def messageBot(self, statu):
+        self.welcoming = ['bonjour', 'Vraiment', 'non', 'Oui']
+        self.successResp = ['et voila', 'ui', 'l\'adresse est la suivante']
+        self.faildResp = ['Malheuresement Pas d\'histoire']
+        self.statu = statu
+        self.msg = ''
+        if self.statu == 'w':
+            self.msg = random.choice(self.welcoming)
+        elif statu == 's':
+            self.msg = random.choice(self.successResp)
+        elif statu == 'f':
+            self.msg = random.choice(self.faildResp)
+        else: 
+            print('Specifie the statu please')
+        return self.msg
--- app/tests/botLogicTests.py
+++ app/tests/botLogicTests.py
@@ -1,1 +1,26 @@
+import unittest
+from app import bot as bt
+
+
+
 #Starting with py bot message list test
+class botTests(unittest.TestCase):
+    """A class to tests all the PyBot's Logics"""
+    
+    def testInlist(self):
+        """test which list is the query in 
+            to make after a best response from 
+            the pybot to the user
+        """
+        self.success = bt.successResp
+        self.faild = bt.faildResp
+        self.wlc = bot.welcoming
+        
+        self.assertIn(bt.messageBot('s'), self.success)
+        self.assertIn(bt.messageBot('f'), self.faild)
+        self.assertIn(bt.messageBot('w'), self.wlc)
+
+
+            
+if __name__ == '__main__':
+    unittest.main()
